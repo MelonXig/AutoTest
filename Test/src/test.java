d30feae01cce2c0d8ad1dcb36a59d4b2ca8aa3b3
MelonXig <505888629@qq.com>
2017-06-24 11:52:41 +0800

    Add files via upload

---
+++ Test/src/test.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class test {
+	public static void main(String[]args)
+	{
+		Map<String,String> hs=new HashMap<String,String>();
+		
+		hs.put("jianren", "maozedong");
+		
+		hs.put("haoren", "maozedong");
+		
+		System.out.println(3/0);
+		
+		
+	}
+}
